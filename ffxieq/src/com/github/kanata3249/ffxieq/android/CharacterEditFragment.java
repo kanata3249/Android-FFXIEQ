50d657d3fff68ee81fe019167049f243d69ff0e0
kanata3249 <kanata3249@gmail.com>
2011-04-22 17:55:25 +0900

    Remove/RemoveAll and WebSearch feature can be invoked from EquipmentSet list.

--- ffxieq/src/com/github/kanata3249/ffxieq/android/CharacterEditFragment.java
+++ ffxieq/src/com/github/kanata3249/ffxieq/android/CharacterEditFragment.java
@@ -17,19 +17,29 @@ package com.github.kanata3249.ffxieq.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+
+import com.github.kanata3249.ffxieq.Equipment;
+import com.github.kanata3249.ffxieq.EquipmentSet;
 import com.github.kanata3249.ffxieq.FFXICharacter;
 import com.github.kanata3249.ffxieq.R;
 
 public class CharacterEditFragment extends FFXIEQFragment {
 	private View mView;
 	private boolean mUpdating;
-    
+	int mLongClickingItemPosition;
+
     @Override
     public void onStart() {
     	super.onStart();
@@ -50,7 +60,18 @@ public class CharacterEditFragment extends FFXIEQFragment {
     					EquipmentSelectorActivity.startActivity(CharacterEditFragment.this, 0, getFFXICharacter(), arg2, ((EquipmentSetView)arg0).getItemId(arg2));
     				}
             	});
-        	}
+            	es.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+					public boolean onItemLongClick(AdapterView<?> arg0,
+							View arg1, int arg2, long arg3) {
+						mLongClickingItemPosition = arg2;
+						getActivity().openContextMenu(arg0);						// TODO Auto-generated method stub
+						return true;
+					}
+            		
+            	});
+    			registerForContextMenu(es);
+    		}
         }
     }
     @Override
@@ -99,7 +120,86 @@ public class CharacterEditFragment extends FFXIEQFragment {
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 
-    public void updateValues() {
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.Remove:
+			getFFXICharacter().setEquipment(mLongClickingItemPosition, -1);
+			updateValues();
+	        if (mListener != null) {
+	    		mListener.notifyDatasetChanged();
+	    	}
+			return super.onContextItemSelected(item);
+		case R.id.RemoveAll:
+			FFXICharacter charInfo = getFFXICharacter();
+			
+			for (int i = 0; i < EquipmentSet.EQUIPMENT_NUM; i++) {
+				charInfo.setEquipment(i, -1);
+			}
+			updateValues();
+	        if (mListener != null) {
+	    		mListener.notifyDatasetChanged();
+	    	}
+			return super.onContextItemSelected(item);
+		}
+
+		// Web Search
+		Equipment eq = getFFXICharacter().getEquipment(mLongClickingItemPosition);
+		Intent intent;
+		if (eq != null) {
+			String[] urls = getResources().getStringArray(R.array.SearchURIs);
+			String name = eq.getName();
+			String url;
+
+			url = null;
+			switch (item.getItemId()) {
+			case R.id.WebSearch0:
+				url = urls[0];
+				break;
+			case R.id.WebSearch1:
+				url = urls[1];
+				break;
+			case R.id.WebSearch2:
+				url = urls[2];
+				break;
+			case R.id.WebSearch3:
+				url = urls[3];
+				break;
+			case R.id.WebSearch4:
+				url = urls[4];
+				break;
+			case R.id.WebSearch5:
+				url = urls[5];
+				break;
+			case R.id.WebSearch6:
+				url = urls[6];
+				break;
+			case R.id.WebSearch7:
+				url = urls[7];
+				break;
+			default:
+				url = null;
+				break;
+			}
+			if (url != null) {
+				intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url + Uri.encode(name.split("\\+")[0])));
+				startActivity(intent);
+				return true;
+			}
+		}
+		return super.onContextItemSelected(item);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+
+		MenuInflater inflater = getActivity().getMenuInflater();
+		inflater.inflate(R.menu.equipmentset_context, menu);
+	}
+
+	public void updateValues() {
     	FFXICharacter charInfo = getFFXICharacter();
 
         if (mUpdating)
