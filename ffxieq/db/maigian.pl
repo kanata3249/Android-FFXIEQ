0e7f7869d364317ec0c08cc047150dbcec13d9b1
kanata3249 <kanata3249@gmail.com>
2013-04-26 06:33:58 +0900

    Import English equipment database.   (Some equipment data has not been translated yet.)   (Food/Atma/Bluemagic/Set-bonus/Jobtraits are not imported.)

--- ffxieq/db/maigian.pl
+++ ffxieq/db/maigian.pl
@@ -1,10 +1,20 @@
 ﻿use DBI;
+use Encode;
 
 my $dbname='master.db';
 my $datasource="dbi:SQLite:dbname=$dbname";
 my $dbh = DBI->connect($datasource);
 
-#reve();					# reverse engineering	Equipment => Maigian
+$lang = "jp";
+$lb = "（";
+$rb = "）";
+removeold();				# remove old data		(delete maijian data in Equipment)
+cre();						# build					Maijian => Equipment
+
+$lang = "en";
+$lb = "(";
+$rb = ")";
+
 removeold();				# remove old data		(delete maijian data in Equipment)
 cre();						# build					Maijian => Equipment
 
@@ -19,82 +29,44 @@ exit;
 
 sub cre
 {
-	my $sth = $dbh->prepare("select * from Maigian order by _id");
+	my $sth = $dbh->prepare("select * from Magian_$lang order by _id");
 	my $nid;
 	my @row;
 
+local $| = 1;
 	$sth->execute();
 	$nid = 40000;
 	while (@row = $sth->fetchrow_array) {
-		my ($mid, $prefix, $postfix, $name, $level, $adesc) = @row;
-		$bsth = $dbh->prepare("select * from Equipment where Name LIKE '$name' AND Lv='$level'");
+		my ($mid, $baseID, $prefix, $postfix, $name, $level, $adesc) = @row;
+		$name =~ s/\'/\'\'/g;
+		$bsth = $dbh->prepare("select * from Equipment_$lang where Name LIKE '$name' AND Lv='$level'");
 		$bsth->execute();
 		if (@row = $bsth->fetchrow_array) {
 			my ($id, $name, $part, $weapon, $job, $race, $level, $rare, $ex, $desc_orig, $desc) = @row;
 			
-			$name = "$name（$prefix$mid$postfix）";
+			$name = "$name$lb$prefix$mid$postfix$rb";
+			$name =~ s/\'/\'\'/g;
 			$desc =~ s/[\r\n]+/\n/g;
 			if (length($adesc) > 0) {
 				$desc =~ s/\s+$//g;
 				$desc = "$desc\n$adesc";
 			}
+			$desc =~ s/\'/\'\'/g;
+			$desc_orig =~ s/\'/\'\'/g;
 #			$desc =~ s/\n/\\n/g;
 #			$desc_orig = $desc;
 			$nid = $mid + 40000;
-			print "$mid $name $level\n";
-			$dbh->do("Insert into Equipment (_id, Name, Part, Weapon, Job, Race, Lv, Rare, Ex, DescriptionOrg, Description) VALUES ('$nid', '$name', '$part', '$weapon', '$job', '$race', '$level', '$rare', '$ex', '$desc_orig', '$desc')");
+			print Encode::encode('cp932', decode_utf8("$mid $name $level                  \r"));
+			$dbh->do("Insert into Equipment_$lang (_id, Name, Part, Weapon, Job, Race, Lv, Rare, Ex, DescriptionOrg, Description) VALUES ('$nid', '$name', '$part', '$weapon', '$job', '$race', '$level', '$rare', '$ex', '$desc_orig', '$desc')");
 		}
 	}
 }
 
-
-
-sub reve
-{
-	my $select = "select * from Equipment where Name LIKE '%（%'";
-	my $sth = $dbh->prepare($select);
-	$sth->execute();
-
-	$nid = 20000;
-	while (my @row = $sth->fetchrow_array) {
-		my ($id, $name, $part, $weapon, $job, $race, $level, $rare, $ex, $desc_orig, $desc) = @row;
-		my ($mid);
-		
-		if ($name =~ /^(.*)（([^0-9]*)([0-9]+)([^0-9]*)）/) {
-			$mid = $3;
-			$name = $1;
-			$prefix = $2;
-			$postfix = $4;
-			
-			$desc = substr($desc, length($desc_orig));
-			$desc =~ s/^\s+//g;
-			$desc =~ s/\n/\\n/g;
-		}
-
-		$sth2 = $dbh->prepare("select _id from Equipment where Name LIKE '$name' and Lv='$level'");
-		$sth2->execute();
-		if (@row = $sth2->fetchrow_array) {
-#			print "$level $name found $row[0]\n";
-		} else {
-			$dbh->do("Insert into Equipment (_id, Name, Part, Weapon, Job, Race, Lv, Rare, Ex, DescriptionOrg, Description) VALUES ('$nid', '$name', '$part', '$weapon', '$job', '$race', '$level', '$rare', '$ex', '$desc_orig', '$desc_orig')");
-			$nid++;
-		}
-
-#		print "$mid	$prefix	$postfix	$name\	$level	$desc\n";
-		$sth2 = $dbh->prepare("select _id from Maigian where _id=?");
-		$sth2->execute($mid);
-		if ($sth2->fetchrow_array) {
-			$dbh->do("update Maigian SET _id='$mid', prefix='$prefix', postfix='$postfix', Name='$name', Level='$level', Desc='$desc' where _id='$mid'");
-		} else {
-			$dbh->do("Insert into Maigian VALUES ('$mid', '$prefix', '$postfix', '$name', '$level', '$desc')");
-		}
-	}
-}
 
 sub removeold
 {
-	$dbh->do("delete From Equipment where Name LIKE '%（%'");
-	$dbh->do("delete From Equipment where Name LIKE '%(%'");
+	$dbh->do("delete From Equipment_$lang where Name LIKE '%（%'");
+	$dbh->do("delete From Equipment_$lang where Name LIKE '%(%'");
 }
 
 
