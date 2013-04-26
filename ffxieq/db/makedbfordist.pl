4a2c0a12116b77dc91391f6f6c8a07fd21e96818
kanata3249 <kanata3249@gmail.com>
2013-04-27 00:39:52 +0900

    makedbfordist.pl script for making database for distribute.

---
+++ ffxieq/db/makedbfordist.pl
@@ -0,0 +1,58 @@
+﻿use DBI;
+use Encode;
+use File::Copy 'copy';
+
+my $master = "master.db";
+
+exit main(@ARGV);
+
+
+
+sub main
+{
+	$dbname = "ffxieq_jp.db";
+	
+	copy $master, $dbname;
+	builddb($dbname, "jp", "en");
+
+	$dbname = "ffxieq_en.db";
+	copy $master, $dbname;
+	builddb($dbname, "en", "jp");
+
+	0;
+}
+
+sub builddb
+{
+	my ($dbname, $lang, $lang2) = @_;
+	my $datasource;
+	my $dbh;
+
+	$datasource="dbi:SQLite:dbname=$dbname";
+	$dbh = DBI->connect($datasource);
+
+
+	$dbh->do("drop table JobNames");
+	$dbh->do("drop table JobGroups");
+	$dbh->do("drop table Types");
+	$dbh->do("drop table Parts");
+
+	$dbh->do("drop table Magian_$lang");
+	$dbh->do("drop table Magian_$lang2");
+	$dbh->do("drop table original_$lang");
+	$dbh->do("drop table original_$lang2");
+
+	$dbh->do("drop table Equipment_$lang2");
+	$dbh->do("drop table JobTrait_$lang2");
+	$dbh->do("drop table Strings_$lang2");
+
+# rename
+	$dbh->do("alter table JobTrait_$lang rename to JobTrait");
+	$dbh->do("alter table Equipment_$lang rename to Equipment");
+	$dbh->do("alter table Strings_$lang rename to Strings");
+	
+	$dbh->do("update Equipment set DescriptionOrg = NULL");
+	$dbh->do("update Atma set DescriptionOrg = NULL");
+
+	$dbh->disconnect();
+}
