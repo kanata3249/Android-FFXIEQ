e7ee8bb91d5eba822cf5d31f16e38bca7ed00317
kanata3249 <kanata3249@gmail.com>
2013-04-29 15:15:22 +0900

    Import BlueMagic data.

--- ffxieq/db/makedbfordist.pl
+++ ffxieq/db/makedbfordist.pl
@@ -47,12 +47,16 @@ sub builddb
 	$dbh->do("drop table JobTrait_$lang2");
 	$dbh->do("drop table Strings_$lang2");
 	$dbh->do("drop table Combination_$lang2");
+	$dbh->do("drop table BlueMagic_$lang2");
+	$dbh->do("drop table BlueMagicCombination_$lang2");
 
 # rename
 	$dbh->do("alter table JobTrait_$lang rename to JobTrait");
 	$dbh->do("alter table Equipment_$lang rename to Equipment");
 	$dbh->do("alter table Strings_$lang rename to Strings");
 	$dbh->do("alter table Combination_$lang rename to Combination");
+	$dbh->do("alter table BlueMagic_$lang rename to BlueMagic");
+	$dbh->do("alter table BlueMagicCombination_$lang rename to BlueMagicCombination");
 	
 	$dbh->do("update Equipment set DescriptionOrg = NULL");
 	$dbh->do("update Atma set DescriptionOrg = NULL");
